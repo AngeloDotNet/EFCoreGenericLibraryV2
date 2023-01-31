7e4b5971fc4736afcde3a623346a57aeab97b3d7
Angelo Pirola <angelo9871@gmail.com>
2023-01-31 22:15:18 +0100

    Aggiunte interfacce

---
+++ Data.EFCore.CustomV1/Interfaces/IDatabase.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.EFCore.CustomV1.Interfaces;
+
+public interface IDatabase<T> where T : class
+{
+    Task<List<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetByIdGuidAsync(Guid id);
+}
